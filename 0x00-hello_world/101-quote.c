2eeafac13627f431bfc131d508f1721a778fa34b
teruis <mohamadelassouli2002@gmail.com>
2023-06-15 13:36:39 -0700

    firC

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,20 @@
+#include <stdio.h>
 #include <unistd.h>
 
-int main() {
-  write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 57);
-  return 1;
-}
+/**
+ * main - Entry point.
+ *
+ * You are not allowed to use any functions listed in the NAME section,
+ * of the man (3) printf or man (3) puts.
+ * Your program should return 1.
+ * Your program should compile without any warnings when using the -Wall,
+ * gcc option.
+ *
+ * Return: 0
+*/
 
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,14 +5,20 @@
  * Description: prints "with proper grammar, but the outcome is a piece of art"
  * Return: 0
 */
-int main() {
-  printf("The size of char is: %ld byte(s)\n", sizeof(char));
-  printf("The size of int is: %ld byte(s)\n", sizeof(int));
-  printf("The size of long is: %ld byte(s)\n", sizeof(long));
-  printf("The size of long long is: %ld byte(s)\n", sizeof(long long));
-  printf("The size of float is: %ld byte(s)\n", sizeof(float));
-  printf("The size of double is: %ld byte(s)\n", sizeof(double));
-  printf("The size of long double is: %ld byte(s)\n", sizeof(long double));
-  printf("The size of void * is: %ld byte(s)\n", sizeof(void *));
-  return 0;
+
+int main(void)
+{
+	char e;
+	int f;
+	long int b;
+	long long int c;
+	float d;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(e));
+	printf("Size of an int: %d byte(s)\n", sizeof(f));
+	printf("Size of a long int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a float: %d byte(s)\n", sizeof(d));
+
+	return (0);
 }
