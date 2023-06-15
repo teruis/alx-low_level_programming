e7349f1e9551618d9fb8b919f021678aa836ca0e
teruis <mohamadelassouli2002@gmail.com>
2023-06-15 13:39:39 -0700

    Firtsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,8 +9,8 @@
  *
  * Return: 0
 */
-int main() {
-  puts("Programming is like building a multilingual puzzle");
-  puts("");
-  return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
