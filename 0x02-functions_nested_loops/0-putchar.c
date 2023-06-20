062b939a6ec3b173c3c6aa7f2ac92c5d4894a08f
teruis <mohamadelassouli2002@gmail.com>
2023-06-20 04:49:12 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,21 @@
 
 /**
  * main - Entry point
+ * 
  * Description: prints _putchar followed by new line
+ *
  * Return: 0
 */
 
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int i;
+	char str[] = "_putchar";
+	int ch;
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(str[i]);
-	}
+	for (ch = 0; ch < 8; ch++)
+	
+		_putchar(str[ch]);
 	_putchar('\n');
-	return (0);
+
+ 	return (0);
 }
