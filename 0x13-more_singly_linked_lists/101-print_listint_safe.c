8e965936d93731f1db810fd51d583335ccb33914
teruis <mohamadelassouli2002@gmail.com>
2023-07-31 15:20:27 -0700

    x

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,26 @@
-#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h" // Assuming you have a header file with the definition of listint_t
 
-/**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: Pointer to the head of the linked list.
- *
- * Return: The number of nodes in the list.
- */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *current = head;
     size_t count = 0;
+    const listint_t *current = head;
 
     while (current != NULL)
     {
         printf("[%p] %d\n", (void *)current, current->n);
         count++;
-        if (current <= current->next)
+
+        if (current >= current->next)
         {
             printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-            break;
+            printf("Error: linked list is not properly terminated\n");
+            exit(98);
         }
+
         current = current->next;
     }
 
-    return (count);
+    return count;
 }
