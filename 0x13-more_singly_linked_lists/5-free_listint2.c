70559ca14a284e1985adacfb106411ad3381f890
teruis <mohamadelassouli2002@gmail.com>
2023-07-31 09:38:34 -0700

    x

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,15 +4,19 @@
  * free_listint2 - frees a linked list
  * @head: pointer to the listint_t list to be freed
  */
-void free_listint2(listint_t **head) {
-    listint_t *current = *head;
-    listint_t *next;
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
 
-    while (current != NULL) {
-        next = current->next;
-        free(current);
-        current = next;
-    }
+	if (head == NULL)
+		return;
 
-    *head = NULL;
+	while (*head)
+	{
+		current = (*head)->next;
+		free(*head);
+		*head = current;
+	}
+
+	*head = NULL;
 }
