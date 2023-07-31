78731d5eb46ba9a5e1e068999feb949e947e14d4
teruis <mohamadelassouli2002@gmail.com>
2023-07-31 16:22:26 -0700

    x

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,14 +9,18 @@
 listint_t *reverse_listint(listint_t **head)
 {
     listint_t *prev = NULL;
+    listint_t *current = *head;
+    listint_t *next = NULL;
 
-    while (*head)
+    while (current != NULL)
     {
-        listint_t *next = (*head)->next;
-        (*head)->next = prev;
-        prev = *head;
-        *head = next;
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
     }
 
-    return (prev);
+    *head = prev;
+
+    return (*head);
 }
