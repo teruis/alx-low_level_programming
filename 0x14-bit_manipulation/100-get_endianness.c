c96ad557fa564902075b31c1a8cf76b8f5a4905f
teruis <mohamadelassouli2002@gmail.com>
2023-08-03 11:42:53 -0700

    x

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,10 +2,12 @@
 
 /**
  * get_endianness - checks the endianness
+ *
  * Return: 0 if big endian, 1 if little endian
- */
-int get_endianness(void) {
-    unsigned int x = 1;
+*/
+int get_endianness(void)
+{
+    unsigned long int x = 1;
 
     return (*(char *)&x);
 }
