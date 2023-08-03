66b6dd006ddc790bcef141db0a73b38ce4104a66
teruis <mohamadelassouli2002@gmail.com>
2023-08-03 11:29:32 -0700

    x

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,8 +8,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= sizeof(*n) * 8)
+    if (index >= sizeof(n) * 8)
         return (-1);
-    *n &= ~(1UL << index);
+        
+    if (*n & 1L << index);
+    		*n ^= 1L << index
     return (1);
 }
