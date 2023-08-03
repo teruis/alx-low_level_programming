07e5db0d18c467e0b9f28bdff45e931647df52c0
teruis <mohamadelassouli2002@gmail.com>
2023-08-03 11:01:18 -0700

    x

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,7 +7,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int nums = 0;
+    unsigned int num = 0;
 
     if (!b)
         return (0);
@@ -17,9 +17,9 @@ unsigned int binary_to_uint(const char *b)
         if (*b != '0' && *b != '1')
             return (0);
 
-        nums = nums * 2 + (*b - '0');
+        num = num * 2 + (*b - '0');
         b++;
     }
 
-    return (nums);
+    return (num);
 }
