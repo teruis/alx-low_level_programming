2cee29528934b6464f7986283020dbb3bc309c64
teruis <mohamadelassouli2002@gmail.com>
2023-08-07 13:15:54 -0700

    x

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,27 +9,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    int res;
+	int fd, w, len = 0;
 
-    if (filename == NULL)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return -1;
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    if (text_content)
-    {
-        res = write(fd, text_content, strlen(text_content));
-        if (res == -1)
-        {
-            close(fd);
-            return -1;
-        }
-    }
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-    close(fd);
+	if (fd == -1 || w == -1)
+		return (-1);
 
-    return 1;
+	close(fd);
+
+	return (1);
 }
